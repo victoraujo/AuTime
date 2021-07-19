94bcf15c06bec2c27415607607ce2d394f55a058
Victor <avieira.victor@gmail.com>
2021-07-19 10:23:26 -0300

    Recreating project

---
+++ AuTime/AuTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuTimeApp.swift
+//  AuTime
+//
+//  Created by Victor Vieira on 19/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AuTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
