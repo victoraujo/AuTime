d1ee5b203ef0b7240ae6e3ae9bd3ee6b3dc991ec
Matheus Andrade <mvtna@cin.ufpe.br>
2021-08-06 12:32:11 -0300

    refactor(sub/activities): Activities and SubActivities list as State var

--- AuTime/View/Subviews/DailyActivitiesView.swift
+++ AuTime/View/Subviews/DailyActivitiesView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct DailyActivitiesView: View {
     @ObservedObject var activitiesManager = ActivityViewModel.shared
+    @State var todayActivities: [Activity] = []
     @Binding var currentActivity: Int
     @Binding var activityReference: Activity?
+    
+    init(currentActivity: Binding<Int>, activityReference: Binding<Activity?>) {
+        self._currentActivity = currentActivity
+        self._activityReference = activityReference
+        self.todayActivities = self.activitiesManager.todayActivities
+    }
     
     var body: some View {
             ScrollView(.horizontal, showsIndicators: false) {
@@ -23,21 +30,22 @@ struct DailyActivitiesView: View {
                             .foregroundColor(.clear)
                             .id(0)
                         
-                        ForEach(Array(self.activitiesManager.todayActivities.enumerated()), id: \.offset) { index, activity in
+                        ForEach(Array(self.todayActivities.enumerated()), id: \.offset) { index, activity in
                             VStack {
                                 ActivityView(activity: activity, colorTheme: .greenColor)
                                     .frame(width: UIScreen.main.bounds.width*0.3, height: UIScreen.main.bounds.height*0.3, alignment: .center)
                                     .background(Rectangle().fill(Color.white).cornerRadius(21).shadow(color: .black90Color, radius: 5, x: 0, y: 6))
-                                    .padding(.bottom)
                                     .onTapGesture {
-                                        activityReference = activity
+                                        if activity.stepsCount > 0 {
+                                            activityReference = activity
+                                        }                                                                    
                                     }
                                 
                                 Text("\(DateHelper.getHoursAndMinutes(from: activity.time))")
                                     .font(.title2)
                                     .fontWeight(.bold)
                                     .foregroundColor(.black90Color)
-                                    .padding(.top, 30)
+                                    .padding(.top, 100)
                             }
                             .id(index + 1)
                             
@@ -46,7 +54,7 @@ struct DailyActivitiesView: View {
                         Rectangle()
                             .frame(width: UIScreen.main.bounds.width*0.3, height: UIScreen.main.bounds.height*0.3, alignment: .center)
                             .foregroundColor(.clear)
-                            .id(self.activitiesManager.todayActivities.count + 1)
+                            .id(self.todayActivities.count + 1)
                         
                     }
                     .onAppear {
@@ -54,8 +62,10 @@ struct DailyActivitiesView: View {
                     }
                     .animation(.easeInOut)
                 }
-                
             }
             .frame(width: UIScreen.main.bounds.width, alignment: .center)
+            .onChange(of: self.activitiesManager.todayActivities, perform: { _ in
+                self.todayActivities = self.activitiesManager.todayActivities
+            })
     }
 }
