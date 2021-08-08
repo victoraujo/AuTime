eec59f1f46454fbfef7b3caf0c79f11916810e0a
Matheus Andrade <mvtna@cin.ufpe.br>
2021-08-07 21:01:59 -0300

    feat(dailyActivities): default first activity is the last

--- AuTime/View/Subviews/DailyActivitiesView.swift
+++ AuTime/View/Subviews/DailyActivitiesView.swift
@@ -71,12 +71,12 @@ struct DailyActivitiesView: View {
             .onAppear {
                 self.todayActivities = self.activitiesManager.todayActivities
 
-                let index = self.todayActivities.lastIndex(where: { !DateHelper.datesMatch(Date(), $0.complete) }) ?? self.todayActivities.count
+                let index = self.todayActivities.lastIndex(where: { !DateHelper.datesMatch(Date(), $0.complete) }) ?? self.todayActivities.count - 1
                 self.currentActivity = index + 1
             }
             .onChange(of: self.activitiesManager.todayActivities, perform: { _ in
                 self.todayActivities = self.activitiesManager.todayActivities
-                let index = self.todayActivities.lastIndex(where: { !DateHelper.datesMatch(Date(), $0.complete) }) ?? self.todayActivities.count
+                let index = self.todayActivities.lastIndex(where: { !DateHelper.datesMatch(Date(), $0.complete) }) ?? self.todayActivities.count - 1
                 self.currentActivity = index + 1
             })
     }
