4b3d05c1560fa6e0be756f9f0e26f32aa5fec168
Victor <avieira.victor@gmail.com>
2021-11-11 11:26:17 -0300

    feat: add premium viewmodel

---
+++ AuTime/ViewModel/PremiumViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  PremiumViewModel.swift
+//  AuTime
+//
+//  Created by Victor Vieira on 09/11/21.
+//
+
+import Foundation
+import Firebase
+import SwiftUI
+
+class PremiumViewModel: ObservableObject {
+    public static var shared = PremiumViewModel()
+    
+    @ObservedObject var userManager = UserViewModel.shared
+    @Published var premiumCount : Int = 0
+    var db = Firestore.firestore()
+    
+    
+    init(){
+        fetchPremium()
+    }
+    
+    func fetchPremium(){
+        if let docId = userManager.session?.email {
+            db.collection("users").document(docId).collection("premium").document("premium").addSnapshotListener { snapshot, err in
+                guard let document = snapshot else {
+                    return
+                }
+                let data  = document.data()
+                self.premiumCount = data!["count"] as? Int ?? 0
+            }
+        }
+    }
+    
+    private func setStarsTo(_ stars: Int){
+        if let docId = userManager.session?.email {
+            db.collection("users").document(docId).collection("premium").document("premium").updateData(["count" : stars])
+        }
+    }
+    
+    func gainStar(){
+        if(premiumCount < 3) {
+            setStarsTo(premiumCount+1)
+        }
+    }
+    
+    func resetStars(){
+        setStarsTo(0)
+    }
+}
